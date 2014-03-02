46ece2ebb10d257e82c0d594c9077af9633072c6
A M <aliosha777@hotmail.com>
2014-03-02 14:27:37 -0500

    Added autofixture customization

---
+++ Banking/Banking.Tests/BankingCustomization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Tests
+{
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Kernel;
+
+    public class BankingCustomization : CompositeCustomization
+    {
+    }
+
+    public class AccountCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customizations.Add(new AccountBuilder());
+        }
+    }
+
+    public class AccountBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            return null;
+        }
+    }
+}
